452512e8714ca819f23d0f4b095379f832ab519a
Kirill Skibin <mineanimatior@gmail.com>
2021-03-13 20:40:08 +0300

    Fixes

--- synthesize.py
+++ synthesize.py
@@ -73,8 +73,8 @@ def synthesize():
 
         model = None
         if hp.phase_reconstruction == True:
-            get_custom_objects().update({'GLU': Activation(GLU)})
-            model = keras.models.load_model(hp.phasemodeldir,custom_objects={ 'loss': custom_loss})
+            #get_custom_objects().update({'GLU': })
+            model = keras.models.load_model(hp.phasemodeldir,custom_objects={ 'loss': custom_loss, 'GLU' : Activation(GLU)})
         
         # Generate wav files
         if not os.path.exists(hp.sampledir): os.makedirs(hp.sampledir)
