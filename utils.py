e514579d3edf4b09fed0ce77f1f54646335366c8
Kirill Skibin <mineanimatior@gmail.com>
2021-03-14 12:31:13 +0300

    Fixes

--- utils.py
+++ utils.py
@@ -95,7 +95,7 @@ def spectrogram2wav(mag, model):
     mag = np.power(10.0, mag * 0.05)
 
     # wav reconstruction
-    wav = griffin_lim(mag**hp.power)
+    wav = fast_griffin_lim(mag**hp.power,phs)
 
     # de-preemphasis
     wav = signal.lfilter([1], [1, -hp.preemphasis], wav)
